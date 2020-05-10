7042368b2c419087f0d6e827f5016760ec6a4462
Peter Simeth <peter@simeth.net>
2020-05-10 16:42:23 +0700

    Requests with user agent and timeout

--- basic_requests.py
+++ basic_requests.py
@@ -4,7 +4,7 @@ url = 'http://www.heise.de'
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36' }
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, timeout=3.05)
 
 with requests.get(url) as resp:
     print(resp.status_code)
